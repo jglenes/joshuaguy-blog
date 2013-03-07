3be62f2a3a925441ea473efa6f77800c6af8c0b9
Joshua Guy <jglenes@gmail.com>
2013-03-07 15:44:04 -0500

    Blog home template structure

---
+++ wp-content/themes/joshuaguy_blog/templates/blog-home-header.php
@@ -0,0 +1,9 @@
+<?php get_template_part('templates/header'); ?>
+
+<div class="container">
+    <div class="page-header">
+      <h1>
+        <?php echo roots_title(); ?>
+      </h1>
+    </div>
+</div>
