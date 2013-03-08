4d80b206428b6e4115ea53f72711c1bf74498622
Joshua Guy <jglenes@gmail.com>
2013-03-07 23:12:47 -0500

    Sidebar

--- wp-content/themes/joshuaguy_blog/base-blog-home.php
+++ wp-content/themes/joshuaguy_blog/base-blog-home.php
@@ -3,28 +3,25 @@
 
   <!--[if lt IE 7]><div class="alert">Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</div><![endif]-->
 
-  <?php
-    do_action('get_header');
-    // Use Bootstrap's navbar if enabled in config.php
-    if (current_theme_supports('bootstrap-top-navbar')) {
-      get_template_part('templates/header-top-navbar');
-    } else {
-      get_template_part('templates/blog-home','header');
-    }
-  ?>
-
-  <div class="wrap container" role="document">
-    <div class="content row">
-      <div class="main <?php echo roots_main_class(); ?>" role="main">
+<div class="wrap" role="document">
+<div class="content row">
+<div class="main <?php echo roots_main_class(); ?>" role="main">
+<div class="container-fluid">
+    <div class="row-fluid">
+        <div class="span3">
+            <!-- Sidebar -->
+            <?php get_template_part('templates/blog','sidebar');?>
+        </div>
+        <div class="span9">
+            <!-- Main Content Area -->
+            <?php get_template_part('templates/blog','content'); ?>
         <?php include roots_template_path(); ?>
-      </div><!-- /.main -->
-      <?php if (roots_display_sidebar()) : ?>
-      <aside class="sidebar <?php echo roots_sidebar_class(); ?>" role="complementary">
-        <?php include roots_sidebar_path(); ?>
-      </aside><!-- /.sidebar -->
-      <?php endif; ?>
-    </div><!-- /.content -->
-  </div><!-- /.wrap -->
+        </div>
+    </div>
+</div> <!-- /.container-fluid -->
+</div><!-- /.main -->
+</div><!-- /.content -->
+</div><!-- /.wrap -->
 
   <?php get_template_part('templates/footer'); ?>
 
