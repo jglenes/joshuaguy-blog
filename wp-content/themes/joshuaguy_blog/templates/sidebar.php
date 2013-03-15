58612919bce0e0aab74c273c73c4abc334b476f7
Joshua Guy <jglenes@gmail.com>
2013-03-15 18:02:43 -0400

    Moving sidebar

---
+++ wp-content/themes/joshuaguy_blog/templates/sidebar.php
@@ -0,0 +1,15 @@
+<div class="sidebar-fixed">
+    <div class="sidebar-contact">
+        <div class="sidebar-portrait">
+            <a href="/">
+                <img src="<?php echo get_stylesheet_directory_uri().'/assets/i/portrait.jpg' ?>" alt="Joshua Guy">
+            </a>
+        </div>
+        <h4 class="sidebar-name">Joshua Guy</h4>
+        <ul class="sidebar-social-bar">
+           <li><i class="icon-large icon-google-plus-sign"></i></li>
+           <li><i class="icon-large icon-github"></i></li>
+           <li><i class="icon-large icon-envelope"></i></li>
+       </ul> <!-- /.sidebar-social-bar-->
+    </div> <!-- /.sidebar-contact -->
+</div> <!-- /.sidebar sidebar-fixed -->
