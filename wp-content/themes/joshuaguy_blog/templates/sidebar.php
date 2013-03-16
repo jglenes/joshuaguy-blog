a8ee919e52316f22465679137581ad40c3583e0c
Joshua Guy <jglenes@gmail.com>
2013-03-15 23:11:38 -0400

    Icon links

--- wp-content/themes/joshuaguy_blog/templates/sidebar.php
+++ wp-content/themes/joshuaguy_blog/templates/sidebar.php
@@ -7,9 +7,9 @@
         </div>
         <h4 class="sidebar-name">Joshua Guy</h4>
         <ul class="sidebar-social-bar">
-           <li><i class="icon-large icon-google-plus-sign"></i></li>
-           <li><i class="icon-large icon-github"></i></li>
-           <li><i class="icon-large icon-envelope"></i></li>
+           <li><a href="http://gplus.to/joshuaguy"><i class="icon-large icon-google-plus-sign"></i></a></li>
+           <li><a href="http://github.com/jglenes"><i class="icon-large icon-github"></a></i></li>
+           <li><a href=""><i class="icon-large icon-envelope"></a></i></li>
        </ul> <!-- /.sidebar-social-bar-->
     </div> <!-- /.sidebar-contact -->
 </div> <!-- /.sidebar sidebar-fixed -->
