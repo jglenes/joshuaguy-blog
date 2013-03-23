5bafc54968d6fc30e77564ddeb01efc90a6ad62c
Joshua Guy <jglenes@gmail.com>
2013-03-23 19:32:10 -0400

    Mobile header links

--- wp-content/themes/joshuaguy_blog/templates/mobileheader.php
+++ wp-content/themes/joshuaguy_blog/templates/mobileheader.php
@@ -1,4 +1,4 @@
 <ul class="horizontal-list">
-    <li class="portrait"><img src="<?php echo get_stylesheet_directory_uri().'/assets/i/portrait.jpg' ?>" alt="Joshua Guy"></li>
-    <li class="name">Joshua Guy</li>
+    <li class="portrait"><a href="/"><img src="<?php echo get_stylesheet_directory_uri().'/assets/i/portrait.jpg' ?>" alt="Joshua Guy"></a></li>
+    <li class="name"><a href="/">Joshua Guy</a></li>
 </ul>
